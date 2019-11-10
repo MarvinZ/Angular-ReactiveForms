c08192ad41978b261ff4e63d93dd43b5de38ae62
Marvin Zumbado <marvinzzz@gmail.com>
2019-11-10 00:05:19 -0600

    fixed issue with folder names

--- APM/src/app/workorders/workorder.module.ts
+++ APM/src/app/workorders/workorder.module.ts
@@ -8,16 +8,16 @@ import { SharedModule } from '../shared/shared.module';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { WorkorderData } from './data';
 
-import { ProductListComponent } from './example/product-list.component';
+import { ProductListComponent } from './ProductExample/product-list.component';
 import { WorkorderListComponent } from './List/workorder-list.component';
 
-import { ProductDetailComponent } from './example/product-detail.component';
+import { ProductDetailComponent } from './ProductExample/product-detail.component';
 import { WorkorderDetailComponent } from './Detail/workorder-detail.component';
 
-import { ProductEditComponent } from './example/product-edit.component';
+import { ProductEditComponent } from './ProductExample/product-edit.component';
 import { WorkorderEditComponent } from './Edit/workorder-edit.component';
 
-import { ProductEditGuard } from './example/product-edit.guard';
+import { ProductEditGuard } from './ProductExample/product-edit.guard';
 import { WorkorderEditGuard } from './Edit/workorder-edit.guard';
 
 
