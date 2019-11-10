a8b01e756a95c6696267fd0bea4d207148772d97
Marvin Zumbado <marvinzzz@gmail.comm>
2019-11-10 11:46:46 -0600

    started to work on the WO objects

--- APM/src/app/workorders/Models/workorder.ts
+++ APM/src/app/workorders/Models/workorder.ts
@@ -11,3 +11,54 @@ export interface Workorder {
   imageUrl: string;
 }
 
+export interface WorkorderDetail {
+  id: number;
+  workorderNumber: string;
+  alternateWorkorderNumber: string;
+  locationId: string;
+  statusCodeId: string;
+  priorityId: string;
+  createdDate: string;
+  updatedDate: string;
+  targetDate?: string;
+  workTypeId: string;
+  requestTypeId: string;
+  requestCodeId: string;
+  vendorId?: string;
+  DNE: number;
+  comments?: WoComment[];
+  assets?: Asset[];
+}
+
+
+export interface WorkorderSimple {
+  id: number;
+  workorderNumber: string;
+  alternateWorkorderNumber?: string;
+  location: string;
+  statusCode: string;
+  priority: string;
+  createdDate: string;
+  updatedDate: string;
+  targetDate: string;
+  workType: string;
+  requestType: string;
+  requestCode: string;
+  vendor: string;
+  DNE: number;
+}
+
+
+export interface Asset {
+  id: number;
+  assetTag: string;
+}
+
+
+
+export interface WoComment {
+  id: number;
+  subject: string;
+  body: string;
+}
+
